19f9484f00311c6cb133e6de8c3333f61fe7677a
刘祖彻 <706716852@qq.com>
2024-02-09 01:09:49 +0800

    登录页面修改

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import '@/assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from '@/App.vue'
 import router from '@/router'
 
 const app = createApp(App)
-
+// 引入elementUI图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(createPinia())
 app.use(router)
 
